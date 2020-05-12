c55cdd47c12f0417a8ae2a8e9ac80f559f909cd9
Furuya-Koichi <seawasekoichi417@icloud.com>
2020-05-12 18:08:37 +0900

    ユーザー編集ページのViewを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to "/users/#{current_user.id}"
     else
       render :edit
     end
